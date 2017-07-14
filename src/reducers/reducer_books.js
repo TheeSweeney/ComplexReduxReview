cd9c9f49a96ec3b996111a00d76087715bd88442
Brian Sweeney <brian.sweeney@cn-brian.sweeney-1740-mbpr>
2017-07-14 13:01:10 -0400

    wrote first container and first reducer (books reducer); connected them to the apps an successfully rendered stuff to the page;

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+ 	return [
+ 		{ title: 'Javascript: The Good Parts'},
+ 		{ title: 'Reuse Reduce Recycle'},
+ 		{ title: 'C-C-C-Complex'},
+ 		{ title: 'History of Hip-Hop Vol. 4'}
+ 	]
+ }
