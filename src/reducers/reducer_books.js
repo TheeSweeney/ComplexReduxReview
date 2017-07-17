1ff0846207f8fde74f4b7963fe3f306d735f289a
Brian Sweeney <brian.sweeney@cn-brian.sweeney-1740-mbpr>
2017-07-17 10:43:58 -0400

    finished implementing book detail component. MVP

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
  	return [
- 		{ title: 'Javascript: The Good Parts'},
- 		{ title: 'Reuse Reduce Recycle'},
- 		{ title: 'C-C-C-Complex'},
- 		{ title: 'History of Hip-Hop Vol. 4'}
+ 		{ title: 'Javascript: The Good Parts', pages: 123},
+ 		{ title: 'Reuse Reduce Recycle', pages: 432},
+ 		{ title: 'C-C-C-Complex', pages: 444},
+ 		{ title: 'History of Hip-Hop Vol. 4', pages: 1093}
  	]
  }
